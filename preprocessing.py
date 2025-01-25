0ec7217e0e49f4d3740a9e351f38ab309ae19ea7
jaysaavedra18 <jasonsaavedra@yahoo.com>
2025-01-24 23:22:50 -1000

    buggy preproccessing but working on it

--- preprocessing.py
+++ preprocessing.py
@@ -4,12 +4,12 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 import nltk
 
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('stopwords')
 
 def preprocess_text(text):
     # Prepare natural language processing tools
-    nltk.download('punkt')
-    nltk.download('wordnet')
-    nltk.download('stopwords')
     # Process the text through tokenization, stopword removal, and lemmatization
     lemmatizer = WordNetLemmatizer()
     stop_words = set(stopwords.words('english'))
